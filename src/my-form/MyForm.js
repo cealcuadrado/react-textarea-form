0565736d75701d0c532dc92635d4fe7ccfe7375c
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-25 20:01:31 -0300

    Establecido formulario reactivo

--- src/my-form/MyForm.js
+++ src/my-form/MyForm.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 function MyForm() {
+    const [textarea, setTextarea] = useState('Este es un texto por defecto');
+
+    const handleChange = (event) => {
+        setTextarea(event.target.value);
+    };
+
     return (
       <div class="my-form">
         <form>
             <div class="form-group">
                 <label>Text</label>
-                <textarea class="form-control"/>          
+                <textarea class="form-control" value={textarea} onChange={(e) => handleChange(e)}/>
             </div>
         </form>
+        <hr />
+        <p>Textarea: {textarea}</p>
       </div>
     );
 }
